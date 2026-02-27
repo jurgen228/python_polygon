352c5d1b7b917c99d05ba87ef1f0150aff2563f6
coder <yurgenmuller228@gmail.com>
2026-02-27 21:35:19 +0200

    deleted polygon.py exceptions.py and ranges.py

--- exceptions.py
+++
@@ -1,7 +0,0 @@
-try:
-    first = int(input("Enter number:"))
-    second = int(input("Enter number:"))
-    print(first*second)
-except ValueError:
-    print("ERROR: only integer input is allowed")
-
